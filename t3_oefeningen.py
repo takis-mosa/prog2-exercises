95c5065752d0eb71f019eebb827225ab56d61a08
MarnickGoossens <marnick.goossens.04@gmail.com>
2021-04-19 14:02:17 +0200

    Black

--- t3_oefeningen.py
+++ t3_oefeningen.py
@@ -1,27 +1,33 @@
 import math
 
+
 def bmi(gewicht, lengte):
-    resultaat = gewicht / (lengte**2)
+    resultaat = gewicht / (lengte ** 2)
     return resultaat
+
 
 def omtrek_cirkel(r):
     resultaat = 2 * r * math.pi
     return resultaat
 
+
 def oppervlakte_cirkel(r):
-    resultaat = r**2 * math.pi
+    resultaat = r ** 2 * math.pi
     return resultaat
+
 
 def oppervlakte_vierkant(zijde):
-    resultaat = zijde**2
+    resultaat = zijde ** 2
     return resultaat
+
 
 def volume_cilinder(r, h):
-    resultaat = math.pi * r**2 * h
+    resultaat = math.pi * r ** 2 * h
     return resultaat
 
+
 def volume_kubus(zijde):
-    resultaat = zijde**3
+    resultaat = zijde ** 3
     if resultaat < 0:
         resultaat = 0
         return resultaat
