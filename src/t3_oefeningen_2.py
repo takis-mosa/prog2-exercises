c08bfd74bc233034a3c13f6b56aa252ae3ccbdb4
Panagiotis H.M. Issaris <takis@issaris.com>
2021-04-21 01:43:19 +0200

    Add exercises for 2021-04-21

---
+++ src/t3_oefeningen_2.py
@@ -0,0 +1,60 @@
+import math
+
+
+def is_even(x):
+    """Geef True als x even is, anders False"""
+    return True
+
+
+def volume_bol(r):
+    """Return volume bol met straal r"""
+    return 0
+
+
+def oppervlakte_cirkel(r):
+    """Return oppervlakte cirkel met straal r"""
+    # Deze functie kan je gebruiken om het volume van de donut te berekenen.
+    return 0
+
+
+def omtrek_cirkel(r):
+    """Return omtrek cirkel met straal r"""
+    # Deze functie kan je gebruiken om het volume van de donut te berekenen.
+    return 0
+
+
+def volume_donut(r, R):
+    """Return volume donut met straal r en R
+    waarbij r de dikte van de donut is, en R
+    de grootte van de donut.
+    """
+    return 0
+
+
+def stats(punten):
+    """Return het gemiddelde, het maximum, het minimum en het aantal getallen
+    als een lijst met punten gegeven werd."""
+    return 0
+
+
+class Cirkel:
+    def __init__(self, r):
+        self.straal = r
+
+    def omtrek(self):
+        """Return de omtrek van de cirkel met straal r"""
+        return 0
+
+    def oppervlakte(self):
+        """Return de oppervlakte van de cirkel met straal r"""
+        return 0
+
+    def __str__(self):
+        """Return een string zoals aangegeven in de testen"""
+        return ""
+
+
+def pythagoras(a, b):
+    """Return de lengte van de schuine zijde als de lengtes
+    van de rechthoekszijden gegeven zijn door a en b"""
+    return 0
---
+++ src/test_t3_oefeningen_2.py
@@ -0,0 +1,129 @@
+from t3_oefeningen_2 import Cirkel, is_even, pythagoras, stats, volume_bol, volume_donut
+
+
+def test_is_even_1():
+    resultaat = is_even(1)
+    assert resultaat == False
+
+
+def test_is_even_2():
+    resultaat = is_even(2)
+    assert resultaat == True
+
+
+def test_is_even_3():
+    resultaat = is_even(3)
+    assert resultaat == False
+
+
+def test_volume_bol_straal_1():
+    resultaat = volume_bol(1)
+    assert abs(resultaat - 4.1) < 0.1
+
+
+def test_volume_bol_straal_16():
+    resultaat = volume_bol(16)
+    assert abs(resultaat - 17157.2) < 0.1
+
+
+def test_volume_donut_3_7():
+    result = volume_donut(r=3, R=7)
+    expected = 1243.57
+    assert abs(result - expected) < 0.1
+
+
+def test_volume_donut_2_4():
+    result = volume_donut(r=2, R=4)
+    expected = 315.83
+    assert abs(result - expected) < 0.1
+
+
+def test_volume_donut_4_4():
+    result = volume_donut(r=4, R=4)
+    expected = -1
+    assert result == expected
+
+
+def test_stats_1():
+    punten = [5]
+    resultaat = stats(punten)
+    assert resultaat == (5.0, 5, 5, 1)
+
+
+def test_stats_4():
+    punten = [5, 9, 10, 8]
+    resultaat = stats(punten)
+    assert abs(resultaat[0] - 8.0) < 0.1
+    assert resultaat[1:] == (10, 5, 4)
+
+
+def test_stats_6():
+    punten = [5, 0, 9.5, 8, 6, 7]
+    gemiddelde, maximum, minimum, nr = stats(punten)
+    assert abs(gemiddelde - 5.9) < 0.1
+    assert minimum == 0
+    assert maximum == 9.5
+    assert nr == 6
+
+
+def test_cirkel_straal_4():
+    x = Cirkel(4)
+    assert abs(x.omtrek() - 25.13) < 0.1
+    assert abs(x.oppervlakte() - 50.27) < 0.1
+    assert f"{x}" == "cirkel met straal 4"
+
+
+def test_cirkel_straal_5():
+    x = Cirkel(5)
+    assert abs(x.omtrek() - 31.42) < 0.1
+    assert abs(x.oppervlakte() - 78.54) < 0.1
+    assert f"{x}" == "cirkel met straal 5"
+
+
+def test_pythagoros_1():
+    result = pythagoras(a=1, b=1)
+    expected = 1.41
+
+    assert abs(result - expected) < 0.1
+
+
+def test_pythagoros_2():
+    result = pythagoras(a=2, b=2)
+    expected = 2.83
+
+    assert abs(result - expected) < 0.1
+
+
+def test_pythagoros_4_5():
+    result = pythagoras(a=4, b=5)
+    expected = 6.4
+
+    assert abs(result - expected) < 0.1
+
+
+def test_pythagoros_01_03():
+    result = pythagoras(a=0.1, b=0.3)
+    expected = 0.32
+
+    assert abs(result - expected) < 0.1
+
+
+def test_pythagoros_a_neg():
+    result = pythagoras(a=-1, b=3)
+    expected = -1
+
+    assert result == expected
+
+
+def test_pythagoros_b_neg():
+    result = pythagoras(a=1, b=-4)
+    expected = -1
+
+    assert result == expected
+
+
+def test_pythagoros_ab_neg():
+    result = pythagoras(a=-2, b=-3)
+    expected = -1
+
+    assert result == expected
